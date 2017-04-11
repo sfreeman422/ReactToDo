64a662ce0e9100a185ff204c20d9799f987a6308
Steven Freeman <sfreeman422@gmail.com>
2017-04-11 11:52:50 -0400

    Added post, put, delete and get routes for the tasks

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,12 +4,30 @@ module.exports = function(app, passport){
 		res.sendFile('../../public/index.html');
 	});
 
+	//Route to get all tasks for a specific user. 
+	app.get("/api/tasks", function(req, res){
+		var tasks = ["Do a thing", "Do another thing", "Do some more things"];
+		console.log(req.body); 
+		res.json(tasks);
+	});
+
 	//Post route for the entry of tasks
-	app.post('/api/task', function(req, res){
+	app.post('/api/tasks', function(req, res){
 		console.log(req.body);
 		res.end();
-	})
+	});
 
+	//Put route for update of tasks.
+	app.put('/api/tasks', function(req, res){
+		console.log(req.body);
+		res.end();
+	});
+
+	//Delete route for deletion of tasks. 
+	app.delete('/api/tasks', function(req, res){
+		console.log(req.body);
+		res.end();
+	});
 }
 
 
