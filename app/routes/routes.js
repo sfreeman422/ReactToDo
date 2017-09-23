af7459d59a3cc548740b3c947065f1566c856c6e
Steve <sfreeman422@gmail.com>
2017-09-23 10:25:26 -0400

    Adjusted background color to match that of the background

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,33 +1,32 @@
-module.exports = function(app, passport){
-	//Initial route to load the page.  
-	app.get('/', function(req, res){
-		res.sendFile('../../public/index.html');
-	});
+module.exports = function (app, passport) {
+  // Initial route to load the page.  
+  app.get('/', (req, res) => {
+    res.sendFile('../../public/index.html');
+  });
 
-	//Route to get all tasks for a specific user. 
-	app.get("/api/tasks", function(req, res){
-		var tasks = ["Do a thing", "Do another thing", "Do some more things"];
-		console.log(req.body); 
-		res.json(tasks);
-	});
+  // Route to get all tasks for a specific user. 
+  app.get('/api/tasks', (req, res) => {
+    const tasks = ['Do a thing', 'Do another thing', 'Do some more things'];
+    console.log(req.body);
+    res.json(tasks);
+  });
 
-	//Post route for the entry of tasks
-	app.post('/api/tasks', function(req, res){
-		console.log(req.body);
-		res.end();
-	});
+  // Post route for the entry of tasks
+  app.post('/api/tasks', (req, res) => {
+    console.log(req.body);
+    res.end();
+  });
 
-	//Put route for update of tasks.
-	app.put('/api/tasks', function(req, res){
-		console.log(req.body);
-		res.end();
-	});
+  // Put route for update of tasks.
+  app.put('/api/tasks', (req, res) => {
+    console.log(req.body);
+    res.end();
+  });
 
-	//Delete route for deletion of tasks. 
-	app.delete('/api/tasks', function(req, res){
-		console.log(req.body);
-		res.end();
-	});
-}
-
+  // Delete route for deletion of tasks. 
+  app.delete('/api/tasks', (req, res) => {
+    console.log(req.body);
+    res.end();
+  });
+};
 
