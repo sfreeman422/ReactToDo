0103a101f806078e64be0dd168dc9cbc08d96f92
Steve <sfreeman422@gmail.com>
2017-10-19 22:51:33 -0400

    Adjusted the task component to prepare it to listen to onFocus and onMouseOver so that we can animate accordingly and make adjustments to the task if need be.

--- app/components/Children/Task.jsx
+++ app/components/Children/Task.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Task = props => (
-  <div className="row col-xs-offset-4 col-xs-4">
-    <div className="panel" id="task">
-      <p className="taskName">{props.taskMessage}</p>
-    </div>
-  </div>
-);
-
-Task.defaultPropTypes = {
-  taskMessage: '',
-};
-
-Task.propTypes = {
-  taskMessage: PropTypes.string,
-};
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: '',
+    };
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+  }
+  handleFocus(event) {
+    console.log(`You have clicked on ${event.target}`);
+  }
+  handleMouseOver(event) {
+    console.log(`You have moused over ${event.target.id}`);
+  }
+  render() {
+    return (
+      <div className="row col-xs-offset-4 col-xs-4" id="taskContainer">
+        <div className="panel" id="task" onMouseOver={this.handleMouseOver}>
+          <p className="taskName" id="taskName">{this.props.taskMessage}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Task;
